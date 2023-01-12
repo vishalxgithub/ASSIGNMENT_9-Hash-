6d4850cf10ec1bb67d8ee1b05e9b38de0e3ff06c
Vishal Gupta <114002191+vishalxgithub@users.noreply.github.com>
2023-01-12 21:07:04 +0530

    Create intersection_of_two_linked_list.cpp

---
+++ intersection_of_two_linked_list.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> s; 
+        ListNode* temp1 = headA;
+        while(temp1 != NULL){
+            s.insert(temp1);
+            temp1 = temp1->next;
+        }
+        ListNode* temp2 = headB;
+        while(temp2 != NULL){
+            if(s.find(temp2) != s.end())
+                return temp2;
+            temp2 = temp2->next;
+        }
+        return NULL;
+    }
